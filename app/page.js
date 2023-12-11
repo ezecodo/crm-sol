091cb33067aa78c789ebe877643277a8d223f37e
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-12-11 17:41:50 -0300

    Header added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
+      <Header /> {/* Componente Header incluido aquí */}
       {/* Contenido de la página */}
-      <Footer />
+      <Footer /> {/* Componente Footer al final */}
     </div>
   );
 }
